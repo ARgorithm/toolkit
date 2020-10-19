64eb2c6b784a255fd52947d68dcb21abe9d6f712
Yatharth <yatharth.mathur@gmail.com>
2020-10-19 15:40:07 +0530

    added tests

---
+++ tests/test_priorityqueue.py
@@ -0,0 +1,38 @@
+import ARgorithmToolkit
+
+algo = ARgorithmToolkit.StateSet()
+queue = ARgorithmToolkit.PriorityQueue("pq",algo)
+
+def test_declare():
+    last_state = algo.states[-1]
+    assert last_state.content["state_type"] == "priorityqueue_declare"
+
+def test_operations():
+    
+    queue.offer(9)
+    queue.offer(3)
+    queue.offer(7)
+    assert queue.body[0] == 3
+   
+    last_state = algo.states[-1]
+    assert last_state.content["state_type"] == "priorityqueue_offer"
+    
+    assert priorityqueue.peek() == 3
+    last_state = algo.states[-1]
+    assert last_state.content["state_type"] == "priorityqueue_peek"
+
+    assert queue.peek() == queue.poll()
+    last_state = algo.states[-1]
+    assert last_state.content["state_type"] == "priorityqueue_poll"
+    queue.poll()
+    assert queue.body == [9]
+
+    try:
+        queue.pop()
+    except ARgorithmToolkit.ARgorithmError:
+        pass
+
+def test_size():
+    assert queue.empty() and len(queue)==0
+
+    
