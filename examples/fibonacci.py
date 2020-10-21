07003735122c6184a521660cb42b0964e4f65b22
Yatharth <yatharth.mathur@gmail.com>
2020-10-21 11:11:26 +0530

    Added array __getitem__ method and fixed fibonacci example need testing

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -4,17 +4,12 @@ import ARgorithmToolkit
 def run(**kwargs):
     n = kwargs["n"]
     algo = ARgorithmToolkit.StateSet()
-    st = ARgorithmToolkit.Stack("stack" , algo , "creating the stack")
-    st.push(0)
-    st.push(1)    
-    curr = ARgorithmToolkit.Variable("answer",algo , comments="this will store the answer")
-    temp = ARgorithmToolkit.Variable("answer",algo , comments="this will store the second highest")
-    if n > 0:
-        for i in range(1,n):
-            temp.value =  st.pop()
-            curr.value = temp.value + st.top()
-            st.push(temp.value)
-            st.push(curr.value)
+    body = [0]*n
+    body[0] = 0
+    body[1] = 1
+    arr = ARgorithmToolkit.Array("arr" , algo ,body, "initializing array")   
+    for i in range(2, n):
+        arr[i] = arr[i-1] + arr[i-2]
     return algo
 
         
